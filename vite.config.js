ac60d1aead1f8c3e6231d76a7cfd46d072f26105
htilssu <tolashuu@gmail.com>
2024-09-13 13:51:53 +0700

    Config module federation vitejs

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import {defineConfig} from 'vite';
+import react from '@vitejs/plugin-react';
+import federation from "@originjs/vite-plugin-federation";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    federation({
+      name: 'remote',
+      remotes: {
+        remote_app: 'https://wowo.htilssu.id.vn/assets/remoteEntry.js',
+      },
+      shared: ['react'],
+    }),
+  ],
+});
